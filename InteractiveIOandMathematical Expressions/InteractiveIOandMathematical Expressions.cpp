62bc5e2b7fb1e9fe024cbb4e24ce5d6f9626be0d
Zoofear <misterz0312@gmail.com>
2024-09-18 18:24:49 -0400

    Tested and Created Project 1

--- InteractiveIOandMathematical Expressions/InteractiveIOandMathematical Expressions.cpp
+++ InteractiveIOandMathematical Expressions/InteractiveIOandMathematical Expressions.cpp
@@ -1,20 +1,46 @@
-// InteractiveIOandMathematical Expressions.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// InteractiveIOandMathematical Expressions.cpp : Project 1 Ingredient Adjuster
+// This project is made to adjust a recipe for the different kind of
 
 #include <iostream>
+#include <iomanip>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+		//Constant variabless for the recipe.
+	
+	const double SugarR = 1.5;	//1.5 cups of sugar needed in the recipe
+	const double ButterR = 1;		//1 cup of butter for the recipe
+	const double FlourR = 2.75;	// 2.75 cups of flour needed for the recipe
+	const double CookieR = 48;	// This recipe makes 48 cookies in this ratio
+
+		//Variables
+
+	int CookieAmount;			//The amount of cookies inputted
+	double CookieRatio;			//Used in a mathematical equation to find out how to alter the formula
+	double Sugar;				//The output post calculations
+	double Butter;				//The output post calculations
+	double Flour;				//The output post calculations
+	
+		//Asking for input
+
+	cout << "How many cookiess do you want to make?" << endl;
+	cin >> CookieAmount;
+	
+		//Calculations
+
+	CookieRatio = CookieAmount / CookieR;	//Divide to get the percentage to use in other calculations
+	Sugar = SugarR * CookieRatio;			//Appling the ratio to the original recipe
+	Butter = ButterR * CookieRatio;			//Appling the ratio to the original recipe
+	Flour = FlourR * CookieRatio;			//Appling the ratio to the original recipe
+
+		//Output
+
+	cout << "To make " << CookieAmount << " amount of cookies, you need:" << endl;
+	cout << setprecision(1) << Sugar << " Cup(s) of Sugar" << endl;
+	cout << setprecision(1) << Butter << " Cup(s) of Butter" << endl;
+	cout << setprecision(1) << Flour << " Cup(s) of Flour" << endl;
+	
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
